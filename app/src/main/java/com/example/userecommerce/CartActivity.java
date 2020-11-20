48a2f8db5a2f15f0d7e35d1a2c2ca5b430636666
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-11-20 23:39:23 +0530

    CartActivity delete and summary functionality

--- app/src/main/java/com/example/userecommerce/CartActivity.java
+++ app/src/main/java/com/example/userecommerce/CartActivity.java
@@ -1,11 +1,12 @@
 package com.example.userecommerce;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.userecommerce.databinding.ActivityCartBinding;
 import com.example.userecommerce.databinding.CartItemBinding;
@@ -17,6 +18,7 @@ import java.util.Map;
 public class CartActivity extends AppCompatActivity {
 
     private ActivityCartBinding b;
+    private CartItemBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +30,25 @@ public class CartActivity extends AppCompatActivity {
 
         for (Map.Entry<String, CartItem> cartItemEntry : cart.cartItemMap.entrySet()) {
 //            Toast.makeText(this, "" + cart, Toast.LENGTH_SHORT).show();
-            CartItemBinding binding = CartItemBinding.inflate(getLayoutInflater());
+            binding = CartItemBinding.inflate(getLayoutInflater());
             binding.productName.setText(cartItemEntry.getKey());
-            binding.productQtyAndPrice.setText(cartItemEntry.getValue().qty + " kg" + " × " + " ₹" + cartItemEntry.getValue().price + "/kg");
+            binding.productQtyAndPrice.setText(cartItemEntry.getValue().qty + " kg" + " × " + " ₹" + Math.ceil(cartItemEntry.getValue().price / cartItemEntry.getValue().qty) + "/kg");
             binding.productTotal.setText("₹ " + cartItemEntry.getValue().price + "");
             b.cartItems.addView(binding.getRoot());
+            setUpDeleteButtons(b, cart, cartItemEntry.getKey());
         }
+    }
+
+    private void setUpDeleteButtons(ActivityCartBinding b, Cart cart, String key) {
+        binding.deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cart.cartItemMap.remove(key);
+                cart.subTotal=0;
+                cart.noOfItems=0;
+                Log.e("main",cart+"");
+                b.cartItems.removeView((View)v.getParent());
+            }
+        });
     }
 }
