2228a3daa4099784dac3d4e1854eee6653cfd3b6
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-11-29 17:40:17 +0530

    Added pendingIntent in notification

--- app/src/main/java/com/example/userecommerce/messaging/MyFcm.java
+++ app/src/main/java/com/example/userecommerce/messaging/MyFcm.java
@@ -2,6 +2,10 @@ package com.example.userecommerce.messaging;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.util.Log;
 
@@ -10,6 +14,7 @@ import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 import com.example.userecommerce.R;
+import com.example.userecommerce.models.Cart;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -17,32 +22,40 @@ import java.util.Random;
 
 public class MyFcm extends FirebaseMessagingService {
 
-    private String CHANNEL_ID="MyFcmClass";
+    private String CHANNEL_ID = "MyFcmClass";
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        String message=remoteMessage.getData().get("versions");
-        Log.e("FCM versions",message+"");
-        Log.e("FCM Title",remoteMessage.getNotification().getTitle());
-        Log.e("FCM Body",remoteMessage.getNotification().getBody());
+        String message = remoteMessage.getData().get("versions");
+        Log.e("FCM versions", message + "");
+        Log.e("FCM Title", remoteMessage.getNotification().getTitle());
+        Log.e("FCM Body", remoteMessage.getNotification().getBody());
 
         createNotificationChannel();
 
-        NotificationCompat.Builder builder=new NotificationCompat.Builder(this,CHANNEL_ID)
+        //Intent code
+        Intent resultIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"));
+        // Create the TaskStackBuilder and add the intent, which inflates the back stack
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addNextIntentWithParentStack(resultIntent);
+        // Get the PendingIntent containing the entire back stack
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.ic_add)
                 .setContentTitle(remoteMessage.getNotification().getTitle())
                 .setContentText(remoteMessage.getNotification().getBody())
                 .setChannelId(CHANNEL_ID)
+                .setContentIntent(resultPendingIntent)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
-        NotificationManagerCompat notificationManager =NotificationManagerCompat.from(this);
-        notificationManager.notify(new Random().nextInt(1000)+1000,builder.build());
-
-
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(new Random().nextInt(1000) + 1000, builder.build());
 
 
     }
+
     private void createNotificationChannel() {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
